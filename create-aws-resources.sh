b33a45040042614b75c3c18a5fd261cf6fdb0630
M shanmukha nath reddy <55055853+LionHuntDiG@users.noreply.github.com>
2024-07-26 17:18:13 +0530

    Update create-aws-resources.sh

--- create-aws-resources.sh
+++ create-aws-resources.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-#Create S3 Bucket to store Kops State
+# Create S3 Bucket to store Kops State
+
 aws s3api create-bucket --bucket kops-s3-bucket-2024  --region us-west-2 --create-bucket-configuration LocationConstraint=us-west-2
+
+# install KOPS
+
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+
 
 # Create KOPS Cluster with 1 control plane with t3.medium and 5 worker nodes with t2.small
 
